a56deff360b8f91ff08b816cb19a711a4cbe7d31
Santiago Varela <sirzes02@gmail.com>
2026-03-31 23:15:24 -0500

    Make a Slime Enemy (part I)

---
+++ enemies/scripts/enemy_state_machine.gd
@@ -0,0 +1,46 @@
+class_name EnemyStateMachine extends Node
+
+var states: Array[State]
+var prev_state: State
+var current_state: State
+
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_DISABLED
+	pass 
+
+func _process(delta: float) -> void:
+	changeState(current_state.process(delta))
+	pass
+	
+func _physics_process(delta: float) -> void:
+	changeState(current_state.physics(delta))
+	pass
+	
+func initialize(_enemy: Enemy) -> void:
+	states = []
+	
+	for child in get_children():
+		if child is State:
+			states.append(child)
+	
+	for state in states:
+		state.enemy = _enemy
+		state.state_machine = self
+		state.init()
+		
+	if states.size() > 0:
+		changeState(states[0])
+		process_mode = Node.PROCESS_MODE_INHERIT
+	
+	pass
+
+func changeState(new_state: State) -> void:
+	if new_state == null || new_state == current_state:
+		return
+		
+	if current_state:
+		current_state.exit()
+		
+	prev_state = current_state
+	current_state = new_state
+	current_state.enter()
---
+++ enemies/scripts/states/enemy_state.gd
@@ -0,0 +1,19 @@
+class_name EnemyState extends State
+
+var enemy: Enemy
+var state_machine: EnemyStateMachine
+
+func init() -> void:
+	pass
+
+func enter() -> void:
+	pass
+	
+func exit() -> void:
+	pass
+	
+func process(_delta: float) -> State:
+	return null
+
+func physics(_delta: float) -> State:
+	return null

79c3df64b79147465b239d1836a3674129158d7d
Santiago Varela <sirzes02@gmail.com>
2026-04-02 11:14:30 -0500

    Player Spawner

--- 00_globals/global_player_manager.gd
+++ 00_globals/global_player_manager.gd
@@ -1,3 +1,31 @@
 extends Node
 
+const PLAYER = preload("uid://b4sse476kdci4")
+
 var player: Player
+var player_spawned: bool = false
+
+func _ready() -> void:
+	add_player_instance()
+	await get_tree().create_timer(0.5).timeout
+	player_spawned = true
+
+func add_player_instance() -> void:
+	player = PLAYER.instantiate()
+	add_child(player)
+	pass
+
+func set_player_position(_new_pos: Vector2) -> void:
+	player.global_position = _new_pos
+	pass
+
+func set_as_parent(_p: Node2D) -> void:
+	if player.get_parent():
+		player.get_parent().remove_child(player)
+	
+	_p.add_child(player)
+	pass
+	
+func unparent_parent(_p: Node2D) -> void:
+	_p.remove_child(player)
+	pass

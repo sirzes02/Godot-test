5f1753170f1318668bc12465440e4fe47d9bad97
Santiago Varela <sirzes02@gmail.com>
2026-04-04 23:20:24 -0500

    Scene Manager

--- levels/scripts/level.gd
+++ levels/scripts/level.gd
@@ -3,3 +3,8 @@ class_name Level extends Node2D
 func _ready() -> void:
 	y_sort_enabled = true
 	PlayerManager.set_as_parent(self)
+	LevelManager.level_load_started.connect(_free_level)
+
+func _free_level() -> void:
+	PlayerManager.unparent_parent(self)
+	queue_free()

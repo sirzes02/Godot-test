107dde2249c1521ac8ff8e2453cba47df16d1f03
Santiago Varela <sirzes02@gmail.com>
2026-04-26 12:14:55 -0500

    Abilities / HUD Display

--- Player/Scripts/abilities/abilities.gd
+++ Player/Scripts/abilities/abilities.gd
@@ -2,20 +2,37 @@ class_name PlayerAbilities extends Node
 
 const BOOMERANG = preload("res://player/boomerang.tscn")
 
-enum abilities { BOOMERANG, GRAPPLE }
+var abilities: Array[String] = [
+	"BOOMERANG", "GRAPPLE", "BOW", "BOMB"
+]
 
-var selected_ability = abilities.BOOMERANG
+var selected_ability: int = 0
 var player: Player
 var boomerang_instance: Boomerang = null
 
 func _ready() -> void:
 	player = PlayerManager.player
+	PlayerHud.update_arrow_count(player.arrow_count)
+	PlayerHud.update_bomb_count(player.bomb_count)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ability"):
-		if selected_ability == abilities.BOOMERANG:
-			boomerang_ability()
-	
+		match selected_ability:
+			0:
+				boomerang_ability()
+			1:
+				pass
+			2: 
+				pass
+			3: 
+				pass
+	elif event.is_action_pressed("switch_ability"):
+		toggle_ability()
+	pass
+
+func toggle_ability() -> void:
+	selected_ability = wrapi(selected_ability + 1, 0, 4)
+	PlayerHud.update_ability_UI(selected_ability)
 	pass
 
 func boomerang_ability()-> void:
